dffef28573cbf15ffd4dcee04eb1f01d67cd0cdf
Sudhanshu777871 <59053168+Sudhanshu777871@users.noreply.github.com>
2020-06-13 20:03:51 +0530

    Silent

---
+++ connect.php
@@ -0,0 +1,22 @@
+<?php
+$firstName=$_POST['firstName'];
+$phoneNumber=$_POST['phoneNumber'];
+$addressShop=$_POST['addressShop'];
+$employesNumber=$_POST['employesNumber'];
+$shopNumber=$_POST['shopNumber'];
+$emailId=$_POST['emailId'];
+$registrationDate=$_POST['registrationDate'];
+$satisfiedFull=$_POST['satisfiedFull'];
+
+
+//DateBase Connection
+
+
+
+
+
+
+
+
+
+?>
